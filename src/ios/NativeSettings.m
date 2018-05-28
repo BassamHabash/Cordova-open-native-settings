e4b8c2650652bda8d5cb39c9dfd668a3f178e6b4
BassamHabash <bassam.habash@exalt.ps>
2018-05-28 12:52:02 +0300

    can't open any custom options on ios11

--- src/ios/NativeSettings.m
+++ src/ios/NativeSettings.m
@@ -17,13 +17,9 @@
 	NSString* prefix = @"App-Prefs:";
 	BOOL result = NO;
 
-	if(SYSTEM_VERSION_LESS_THAN(@"10.0")){
+	if(SYSTEM_VERSION_LESS_THAN(@"10.0") || @available(iOS 11.0, *)){
 		prefix = @"prefs:";
 	}
-	
-	if (@available(iOS 11.0, *)) {
-        	prefix = @"app-settings:";
-    	}
 	
     if ([key isEqualToString:@"application_details"]) {
         result = [self do_open:UIApplicationOpenSettingsURLString];
